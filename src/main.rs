8db8d6cf2e7981d8a5432a2f02d587f67bce96e7
longwatermelon <longwatermelon1@gmail.com>
2023-11-27 17:22:03 -0700

    shrek sprites

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ const MAX_ENTS: usize = 15;
 struct Entities {
     ents: Vec<rc::Entity>,
     speeds: Vec<f32>,
+    death_timers: Vec<Option<f64>>,
 }
 
 impl Entities {
@@ -19,17 +20,20 @@ impl Entities {
         Self {
             ents: Vec::new(),
             speeds: Vec::new(),
+            death_timers: Vec::new(),
         }
     }
 
     fn push(&mut self, ent: rc::Entity, speed: f32) {
         self.ents.push(ent);
         self.speeds.push(speed);
+        self.death_timers.push(None);
     }
 
     fn remove(&mut self, index: usize) {
         self.ents.remove(index);
         self.speeds.remove(index);
+        self.death_timers.remove(index);
     }
 }
 
@@ -55,6 +59,7 @@ async fn main() {
     let mut textures: HashMap<char, mq::Image> = HashMap::new();
     textures.insert('0', mq::Image::from_file_with_format(include_bytes!("res/wall.png"), Some(mq::ImageFormat::Png)).unwrap());
     textures.insert('e', mq::Image::from_file_with_format(include_bytes!("res/shrek.png"), Some(mq::ImageFormat::Png)).unwrap());
+    textures.insert('d', mq::Image::from_file_with_format(include_bytes!("res/shrek_dead_gun.png"), Some(mq::ImageFormat::Png)).unwrap());
     let mut map: rc::Map = rc::Map::from_bytes(include_bytes!("res/map"), textures);
     map.floor_tex(rc::Surface::Color(mq::BEIGE.into()));
     map.ceil_tex(rc::Surface::Color(mq::GRAY.into()));
@@ -126,10 +131,12 @@ async fn main() {
             }
 
             // Raycast
-            let ins: rc::Intersection = rc::cast_ray(&map, ents.ents.iter(), &[], cam);
+            let ins: rc::Intersection = rc::cast_ray(&map, ents.ents.iter(), &['d'], cam);
             match ins.itype {
                 rc::IntersectionType::Entity { index, .. } => {
-                    ents.remove(index);
+                    // ents.remove(index);
+                    ents.death_timers[index] = Some(mq::get_time());
+                    ents.ents[index].texture = 'd';
                     audio.play_sound("death");
                 }
                 _ => (),
@@ -138,7 +145,7 @@ async fn main() {
 
         if mq::is_mouse_button_pressed(mq::MouseButton::Right) {
             grappling = true;
-            grapple_target = cam.along(rc::cast_ray(&map, ents.ents.iter(), &['e'], cam).distance);
+            grapple_target = cam.along(rc::cast_ray(&map, ents.ents.iter(), &['e', 'd'], cam).distance);
             audio.play_sound("grapple");
         }
 
@@ -152,8 +159,26 @@ async fn main() {
             );
         }
 
+        // Remove dead entities
+        for (i, death) in ents.death_timers.iter().enumerate() {
+            if let Some(death) = death {
+                if mq::get_time() - *death > 1. {
+                    // Can't remove multiple ents in a singe loop, just get the rest next frame
+                    ents.remove(i);
+                    break;
+                }
+            }
+        }
+
         // Move entities
-        for (ent, speed) in ents.ents.iter_mut().zip(ents.speeds.iter()) {
+        for (ent, (speed, dead)) in ents.ents.iter_mut()
+                                             .zip(ents.speeds.iter()
+                                             .zip(ents.death_timers.iter()))
+        {
+            if dead.is_some() {
+                continue;
+            }
+
             let diff: Vec2 = cam.orig - ent.pos;
             let theta: f32 = f32::atan2(diff.y, diff.x) + mq::rand::gen_range(-1.5, 1.5);
             let dir: Vec2 = Vec2::new(theta.cos(), theta.sin());
